94f4a9808fd8ed12926c9053cb83dcfec56c90c7
Martina Papp <martinapapp0@gmail.com>
2026-02-12 10:46:36 +0100

    add toTitleCase problem

---
+++ toTitleCase.js
@@ -0,0 +1,13 @@
+/**
+Example Input: "everything, everywhere, all at once"
+Example Output: "Everything, Everywhere, All At Once"
+
+Example Input: "scrimba"
+Example Output: "Scrimba"
+ */
+
+
+
+//test
+console.log(capitalizeWord("pumpkin"))
+console.log(toTitleCase("pumpkin pranced purposefully across the pond"))
