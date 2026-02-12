22f0e0b447100a0c83ffcbe039664d44646cd981
Martina Papp <martinapapp0@gmail.com>
2026-02-12 10:02:55 +0100

    add alternatingCaps function problem

---
+++ alternatingCaps.js
@@ -0,0 +1,9 @@
+/**
+Example input: "I'm so happy it's Monday"
+Example output: "I'M So hApPy iT'S MoNdAy"
+ */
+
+
+
+//test
+console.log(altCaps("When you visit Portland you have to go to VooDoo Donuts"))
