fe0f4be9ff3800ec7a77b3f2cf03223bd20c76c2
Martina Papp <martinapapp0@gmail.com>
2026-02-12 09:45:21 +0100

    solve panic function

--- panicFunction.js
+++ panicFunction.js
@@ -6,7 +6,16 @@ Example input: "I'm almost out of coffee"
 Example output: "I'M 😱 ALMOST 😱 OUT 😱 OF 😱 COFFEE!"
  */
 
+//pseudo: 1. split into words 2. join back separeted with emoji 3. make it uppercase 4. add "!" to the end
 
+function panic(str){
+   return str
+            .split(" ")
+            .join(' 😱 ')
+            .toUpperCase()
+            +'!'
+    
+}
 
 // Test
 console.log(panic("I'm almost out of coffee")); 
