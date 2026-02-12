07f8cc8c669d74701164d38cbccd1696b4812dc7
Martina Papp <martinapapp0@gmail.com>
2026-02-12 15:14:38 +0100

    add reverse srting problem

---
+++ reverseStr.js
@@ -0,0 +1,28 @@
+
+
+/**
+Example input: !htrae ot emocleW
+Example output: Welcome to earth!
+ */
+
+//db
+const title = ":htraE no od ot ffutS";
+const messages = [
+            "maerc eci yrT",
+            "rewoT leffiE tisiV",
+            "noom eht ot snamuh etacoleR",
+            "egrahc ni stac tuP", 
+        ]
+
+
+function reverseString(arr){
+    return arr;
+}
+
+function reverseStringsInArray(arr){
+    return arr;
+}
+
+//test
+console.log(reverseString(title));
+console.log(reverseStringsInArray(messages));
