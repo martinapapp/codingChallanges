580e88805638eb1d009581900d444b48282230b3
Martina Papp <martinapapp0@gmail.com>
2026-02-12 16:07:44 +0100

    add isPalindrom function problem

---
+++ isPalindrome.js
@@ -0,0 +1,17 @@
+/**
+Write a function to check if a lowercased string of letters is a palindrome. 
+If the word is palindrome, return true. If it isn't, return false.
+
+Example input: "motorbike"
+Example output: false
+
+Example input: "rotator" 
+Example output: true
+*/ 
+
+// test
+console.log(isPalindrome("abbA"));
+console.log(isPalindrome("Civic"));
+console.log(isPalindrome("octopus"));
+console.log(isPalindrome("pumpkins"));
+console.log(isPalindrome("madam"));
