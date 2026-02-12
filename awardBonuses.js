a0a8242d327b598e764a354c9269fcb1f372610c
Martina Papp <martinapapp0@gmail.com>
2026-02-12 11:25:33 +0100

    add awardBonuses problem

---
+++ awardBonuses.js
@@ -0,0 +1,22 @@
+/**
+The company has 100 employees and their employee ID numbers range from 1 - 100. 
+If the employee's ID number is: 
+    Divisible by 3 - Vacation! 
+    Divisible by 5 - $100,000 bonus! 
+    Divisible by both 3 and 5 - JACKPOT! 1 Million and a Yacht!
+    Not divisible by 3 or 5 - :(
+output: 
+    1 - :(
+    2 - :(
+    3 - Vacation! 
+    4 - :(
+    5 - $100,000 bonus!
+ */
+
+
+
+function awardBonuses(){
+
+}
+
+awardBonuses();
