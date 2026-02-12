e4c3243f2fdd8cbe9771e7fbc3418dec1fe2b2f8
Martina Papp <martinapapp0@gmail.com>
2026-02-12 11:56:40 +0100

    solve awardBonuses

--- awardBonuses.js
+++ awardBonuses.js
@@ -13,10 +13,33 @@ output:
     5 - $100,000 bonus!
  */
 
+/**pseudo
+ * 1. loop through 1-100 
+ * 2. switch (TRUE)
+ *  2.1. case divisible by 3 and : console "JACKPOT! 1 Million and a Yacht!", break;
+ *  2.2. case divisible by 5: console "$100,000 bonus!", break;
+ *  2.3. case divisble by 3: console "Vacation!", break;
+ *  2.4. default : console ":("
+ */
 
 
 function awardBonuses(){
-
+    for(let i = 1; i <= 100; i++){
+        switch(true){
+            case (i % 3 === 0) : 
+                console.log(`${i} - $100,000 bonus!`) 
+                break
+            case (i % 5 === 0) : 
+                console.log(`${i} - Vacation!`)
+                break
+            case (i % 3 === 0 && i % 5 === 0) : 
+                console.log(`${i} - JACKPOT! 1 Million and a Yacht!`)
+                break
+            default : 
+                console.log(`${i} - :(`) 
+        }
+    }
 }
+
 
 awardBonuses();
