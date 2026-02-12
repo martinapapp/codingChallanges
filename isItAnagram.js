dac9f8efaeee8ff9200aebac85e93a956f5890e3
Martina Papp <martinapapp0@gmail.com>
2026-02-12 12:46:34 +0100

    add isAnagram problem

---
+++ isItAnagram.js
@@ -0,0 +1,13 @@
+/**
+Example input: "allergy", "gallery"
+Example output: true
+
+Example input: "rainbow", "crossbow"
+Example output: false
+ */
+
+
+
+//test
+console.log(isAnagram("allergy", "gallery"))
+console.log(isAnagram("treasure", "measure"))
