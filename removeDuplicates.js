b24db3b08f0417e9a2f06e81943906ed65a3bd45
Martina Papp <martinapapp0@gmail.com>
2026-02-12 16:41:51 +0100

    solve remove duplicates

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -7,9 +7,25 @@ Example input: "aabbccb1212"
 Example output: "abc12"
 */ 
 
+const password = "9338dsabbbadjdjdj2sdfdfdf282ff8fdsd888ss8cfgfg332q23"
+
+/**pseudo:
+* 1. create empty space for password 
+* 2. loop through chars
+*      2.1. check if there isn't the same value in password
+*           2.1.1. true: push it to the password
+* 3. return password
+*/
+
+function removeDupeChars(chars){
+    let pw = ""
+    for(let char of chars){
+        if(!pw.includes(char)) pw +=char 
+    }
+    return pw
+}
 
 
-const password = "9338dsabbbadjdjdj2sdfdfdf282ff8fdsd888ss8cfgfg332q23"; 
  
-
-console.log(removeDupeChars(password));
+//test
+console.log(removeDupeChars(password))
