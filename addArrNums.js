449baf64a12ed16720de7f4b238f13ce78499edb
Martina Papp <martinapapp0@gmail.com>
2026-02-16 11:25:50 +0100

    solve sum array of numbers

--- addArrNums.js
+++ addArrNums.js
@@ -5,8 +5,12 @@ Example output: 6
 
 const studentCount = [50,53,61,67,60,70,78,80,80,81,90,110];
 
+/**pseudo
+ * return sumArr (.reduce())
+ */
+
 function sumArray(arr){
- 
+ return arr.reduce((total, current)=> {return total + current}, 0)
 }
 
 console.log(sumArray(studentCount));
