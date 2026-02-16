97cde5c6f9c5ef10bc87dbee9d31c61f4359a649
Martina Papp <martinapapp0@gmail.com>
2026-02-16 11:22:34 +0100

    add sum array numbers problem

---
+++ addArrNums.js
@@ -0,0 +1,12 @@
+/**
+Example input: [1,2,3]
+Example output: 6
+ */
+
+const studentCount = [50,53,61,67,60,70,78,80,80,81,90,110];
+
+function sumArray(arr){
+ 
+}
+
+console.log(sumArray(studentCount));
