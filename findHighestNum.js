d20c791dae80503385d95c886cb162982ea5cffc
Martina Papp <martinapapp0@gmail.com>
2026-02-16 11:30:12 +0100

    add find highest score in an obj problem

---
+++ findHighestNum.js
@@ -0,0 +1,24 @@
+/**
+Write a function to find the food with the highest number of votes. 
+
+Your function should take in a food object and find the food
+with the most votes. It should log the winner, along with 
+how many votes it received.  
+
+Example input: {"🐈 cats": 19, "🐕 dogs": 17} 
+Example output: The winner is 🐈 cats with 19 votes!
+ */
+
+
+const gameNightFood = {
+    "🍕 pizza": 3, 
+    "🌮 tacos": 10, 
+    "🥗 salads": 7,
+    "🍝 pasta": 5
+}
+
+function findTheWinner(obj){
+
+}
+
+console.log(findTheWinner(gameNightFood));
