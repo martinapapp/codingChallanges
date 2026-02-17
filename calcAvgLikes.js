11226a5cab1257ce8a3988da100d12e8ecb49c55
Martina Papp <martinapapp0@gmail.com>
2026-02-17 11:53:59 +0100

    add get avarage likes problem

---
+++ calcAvgLikes.js
@@ -0,0 +1,44 @@
+/**
+Popularity contest.
+Iggy the Influencer and Toby the Tiktoker are dying to know
+who's more popular on social media. 
+Toby's TikToks get an average of 400 likes. On average, how many
+likes do Iggy's Instagram posts get? 
+ */
+
+//data
+
+const postData = [
+     {
+        post: "A lot of you have asked about my morning skin care routine...",
+        likes: 348
+     },
+     {
+        post: "New Year, new me. #blessed",
+        likes: 102
+     },
+     {
+        post: "Sunday by the pool 🌴 #vibes #mood #hotdoglegs",
+        likes: 544
+     },
+     {
+        post: "Here are my top 5 #notsponsored unbiased dry shampoo recs...",
+        likes: 745
+     },
+     {
+        post: "Last day in Hawaii! The tans will fade but the memories will last forever #holidays #foreverhawaii #blessed #followme",
+        likes: 412
+     },
+     {
+        post: "Leg day #summerbody2022 #gymshark #beachworkout",
+        likes: 275
+     },
+]
+   
+
+function calcAverageLikes(data){
+  
+} 
+
+//test
+console.log(calcAverageLikes(postData))
