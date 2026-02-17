6d955e9be66fd167cd850cdbe3461c2cbba2f0d2
Martina Papp <martinapapp0@gmail.com>
2026-02-17 11:48:59 +0100

    solve sort by duration

---
+++ sortByDuration.js
@@ -0,0 +1,103 @@
+/**
+Our Scrimba Airlines in-flight entertainment package 
+includes a variety of podcasts. We need to add a feature that suggests
+podcasts to our patrons based on whether a flight is short or long. 
+
+Your sort function should take two arguments: the podcast data and
+flight length. If the flight is 60 minutes or less, sort the podcast list 
+from shortest to longest. If it's anything else, sort from longest
+to shortest. 
+
+Your function shouldn't return anything. Instead log a numbered list 
+of the title and duration of 
+each podcast to the console, like this:
+
+1. Crime Fan, 150 minutes
+2. Mythical Creatures, 99 minutes
+3. Crime Crime Crime, 70 minutes
+4. Coding Corner, 55 minutes
+5. Scrimba Podcast, 50 minutes
+6. Something about Witches, 35 minutes
+*/ 
+
+//data
+const podcasts = [
+    {
+        id: 1,
+        title: "Scrimba Podcast", 
+        duration: 50, 
+        tags: ["education", "jobs", "technology"], 
+        hosts: ["Alex Booker"], 
+        rating: 10,
+        genre: "education",
+        paid: false
+    },
+    {
+        id: 2,
+        title: "Crime Fan", 
+        duration: 150, 
+        tags: ["crime", "entertainment", "mature"], 
+        hosts: ["Bob Smith", "Camilla Lambert"], 
+        genre: "true crime",
+        rating: 5, 
+        paid: true
+    },
+    {
+        id: 3,
+        title: "Mythical Creatures", 
+        duration: 99, 
+        tags: ["entertainment", "general", "unicorns"], 
+        hosts: ["Esmerelda Shelley", "Duke Dukington", "Felix the Cat"], 
+        genre: "fantasy",
+        rating: 8,
+        paid: true
+    },
+    {
+        title: "Crime Crime Crime", 
+        duration: 70, 
+        tags: ["crime", "entertainment", "mature"], 
+        hosts: ["Jessica Jones", "Humphrey Bogart", "Inspector Gadget"], 
+        genre: "true crime",
+        rating: 6, 
+        paid: true
+    },
+    {
+        title: "Something about Witches", 
+        duration: 35, 
+        tags: ["fantasy", "entertainment"], 
+        hosts: ["Frewin Wyrm", "Evanora Highmore"], 
+        genre: "fantasy",
+        rating: 8,
+        paid: false
+    },
+    {
+        title: "Coding Corner", 
+        duration: 55, 
+        tags: ["education", "jobs", "technology"], 
+        hosts: ["Treasure Porth", "Guil Hernandez", "Tom Chant"], 
+        genre: "education",
+        rating: 9,
+        paid: false
+    },
+]
+
+/**pseudo
+ * 1.check duration time is or less than 60 
+ *      1.1. true: sort from shortest to longest
+ *      1.2. false: sort from longest to shortest
+ * 2. log as a numbered list
+ */
+
+function sortByDuration(data, flightLength){
+    if(flightLength <= 60){
+        data.sort((a,b)=> a.duration - b.duration)   
+    }else{
+        data.sort((a,b)=> b.duration - a.duration)
+    }
+
+    data.forEach(({title, duration}, index) => {
+    console.log(`${index + 1}. ${title}, ${duration} minutes`)
+    }) 
+}
+
+sortByDuration(podcasts, 30)
